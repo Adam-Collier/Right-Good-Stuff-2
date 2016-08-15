1a0cd322acbdcf0c5c06723e3074aba3affcf1d5
Adam-Collier <adamcollierart@gmail.com>
2016-08-16 00:59:38 +0100

    changed the absolute paths

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
 
-$("a.boom").append("<img src='/img/buttons/boom.png'>");
+$("a.boom").append("<img src='img/buttons/boom.png'>");
 
-$("a.itsnicethat").append("<img src ='/img/buttons/itsnicethat.png'>");
+$("a.itsnicethat").append("<img src ='img/buttons/itsnicethat.png'>");
 
-$("a.designmilk").append("<img src = '/img/buttons/designmilk.png'>");
+$("a.designmilk").append("<img src = 'img/buttons/designmilk.png'>");
 
-$("a.eyeondesign").append("<img src='/img/buttons/eyedesign.png'>");
+$("a.eyeondesign").append("<img src='img/buttons/eyedesign.png'>");
 
-$("a.dezeen").append("<img src='/img/buttons/dezeen.jpg'>");
+$("a.dezeen").append("<img src='img/buttons/dezeen.jpg'>");
 
-$("a.architizer").append("<img src='/img/buttons/architizer.jpg'>");
+$("a.architizer").append("<img src='img/buttons/architizer.jpg'>");
 
-$("a.aisle").append("<img src='/img/buttons/aisleone.png'>");
+$("a.aisle").append("<img src='img/buttons/aisleone.png'>");
 
-$("a.colossal").append("<img src='/img/buttons/colossal.jpg'>");
+$("a.colossal").append("<img src='img/buttons/colossal.jpg'>");
 
-$("a.form").append("<img src='/img/buttons/fff.jpg'>");
+$("a.form").append("<img src='img/buttons/fff.jpg'>");
 
 
 // touch screen click animation
