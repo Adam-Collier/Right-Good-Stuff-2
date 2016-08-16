fde6310d354ac720c9452833b9a7ee576158f74e
Adam-Collier <adamcollierart@gmail.com>
2016-08-16 01:10:17 +0100

    adding new content, testing buttons on hover

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-<<<<<<< HEAD
+
 $("a.boom").append("<img src='img/buttons/boom.png'>");
 
 $("a.itsnicethat").append("<img src ='img/buttons/itsnicethat.png'>");
@@ -18,25 +18,9 @@ $("a.aisle").append("<img src='img/buttons/aisleone.png'>");
 $("a.colossal").append("<img src='img/buttons/colossal.jpg'>");
 
 $("a.form").append("<img src='img/buttons/fff.jpg'>");
-=======
-$("a.boom").append("<img src='/rightgoodstuff/img/buttons/boom.png'>");
 
-$("a.itsnicethat").append("<img src ='/rightgoodstuff/img/buttons/itsnicethat.png'>");
+$("a.guardian").append("<img src='img/buttons/guardian.jpg'>");
 
-$("a.designmilk").append("<img src = '/rightgoodstuff/img/buttons/designmilk.png'>");
-
-$("a.eyeondesign").append("<img src='/rightgoodstuff/img/buttons/eyedesign.png'>");
-
-$("a.dezeen").append("<img src='/rightgoodstuff/img/buttons/dezeen.jpg'>");
-
-$("a.architizer").append("<img src='/rightgoodstuff/img/buttons/architizer.jpg'>");
-
-$("a.aisle").append("<img src='/rightgoodstuff/img/buttons/aisleone.png'>");
-
-$("a.colossal").append("<img src='/rightgoodstuff/img/buttons/colossal.jpg'>");
-
-$("a.form").append("<img src='/rightgoodstuff/img/buttons/fff.jpg'>");
->>>>>>> 479368d5b181a81b852dd4c574aeb2095f3f879f
 
 
 // touch screen click animation
